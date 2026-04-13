53b027239f606f0ed06388e4bba52e959a5cff1e
kurveshnasit-blip <kurvesh.nasit@gmail.com>
2026-04-13 15:55:50 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/tradingapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tradingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Basic entry point
+    }
+}
